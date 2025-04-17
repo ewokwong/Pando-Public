cf6876b8d8bfa22285bed0154448c62c5012d1a1
Ethan Kwong <eonnkwong@atlassian.com>
2025-04-17 23:18:23 +1000

    Banning tiktok agent

--- frontend-next/src/utils/isInAppBrowser.tsx
+++ frontend-next/src/utils/isInAppBrowser.tsx
@@ -1,5 +1,5 @@
 // Utility function to detect if user is in an in-app browser
 export const isInAppBrowser = (): boolean => {
     const userAgent = navigator.userAgent || navigator.vendor;
-    return /FBAN|FBAV|Instagram/.test(userAgent);
+    return /FBAN|FBAV|Instagram|Bytedance/.test(userAgent);
   };
